ce17d610cfec65b012950453077a8ed9c3e62fda
sashasohrabi <shb.sohrabi@gmail.com>
2019-01-31 23:31:20 +0200

    slider section.

--- build/js/main.js
+++ build/js/main.js
@@ -1,10 +1,23 @@
 "use strict";
 
+// Hamburger Menu
 $(function () {
   $("#dl-menu").dlmenu({
     animationClasses: {
       classin: "dl-animate-in-5",
       classout: "dl-animate-out-5"
     }
+  });
+});
+$(document).ready(function () {
+  $(".slider__box").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: false,
+    infinite: false,
+    prevArrow: false,
+    fade: true,
+    cssEase: "linear",
+    nextArrow: false
   });
 });
